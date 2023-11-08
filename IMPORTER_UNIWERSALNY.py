dc4a83e92654afcb23dde841707160d716d94420
pkonieczny007 <50171535+pkonieczny007@users.noreply.github.com>
2023-11-08 11:53:07 +0100

    poprawione - wgrywanie wykazow

--- IMPORTER_UNIWERSALNY.py
+++ IMPORTER_UNIWERSALNY.py
@@ -4,7 +4,43 @@ import json
 import shutil
 
 class UniversalImporter:
-    # ... (pozostałe metody klasy pozostają bez zmian)
+    def __init__(self, script_directory, json_filename):
+        self.script_directory = script_directory
+        self.json_filename = json_filename
+        self.scripts = self.load_scripts()
+
+    def load_scripts(self):
+        json_path = os.path.join(self.script_directory, self.json_filename)
+        with open(json_path, 'r', encoding='utf-8') as file:
+            scripts_data = json.load(file)
+        return scripts_data
+
+    def list_scripts(self):
+        print("Dostępne skrypty:")
+        for index, script in enumerate(self.scripts, start=1):
+            print(f"{index}. {script}")
+        print(f"{len(self.scripts) + 1}. KOPIUJ XML")
+
+    def run_script(self, script_number):
+        if script_number == len(self.scripts) + 1:
+            self.copy_xml_files()
+        else:
+            script_name = list(self.scripts.keys())[script_number - 1]
+            instructions = self.scripts[script_name]
+            print(f"Instrukcje dla skryptu: {instructions}")
+            input("Naciśnij Enter, aby kontynuować...")
+            script_path = os.path.join(self.script_directory, script_name)
+            print(f"Uruchamianie skryptu: {script_name}")
+            subprocess.run(['python', script_path], shell=True)
+            print(f"Zakończono uruchamianie skryptu: {script_name}")
+
+    def copy_xml_files(self):
+        xml_files = [f for f in os.listdir(self.script_directory) if f.endswith('.xml')]
+        for xml_file in xml_files:
+            source_path = os.path.join(self.script_directory, xml_file)
+            destination_path = os.path.join(current_directory, xml_file)
+            shutil.copy2(source_path, destination_path)
+        print("Wszystkie pliki XML zostały skopiowane.")
 
     def copy_file_to_importers(self, filename):
         source_path = os.path.join(current_directory, filename)
@@ -16,10 +52,7 @@ class UniversalImporter:
             print(f"Plik {filename} nie istnieje w bieżącym katalogu.")
 
     def choose_and_run(self):
-        # Wyświetl komunikat powitalny
         print("IMPORTER UNIWERSALNY. UTWÓRZ PLIK wykazy.xlsx")
-
-        # Pytanie o wczytanie pliku wykazy.xlsx
         choice = input("Czy wczytać plik wykazy.xlsx? [y/n]: ").lower()
         if choice == 'y':
             self.copy_file_to_importers('wykazy.xlsx')
@@ -30,7 +63,6 @@ class UniversalImporter:
             print("Nieprawidłowy wybór.")
             return
 
-        # Wyświetl listę dostępnych importerów
         self.list_scripts()
         try:
             script_choice = int(input("Wpisz numer skryptu, który chcesz uruchomić lub 'KOPIUJ XML': "))
